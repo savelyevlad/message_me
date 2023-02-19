9d3d8a7d5b62b65f1466539e35ee62254f61bd07
Vladyslav <savelyevlad@gmail.com>
2023-02-19 19:53:38 +0100

    semantic ui and js apperently works

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
 //= require jquery
+//= require jquery_ujs
+//= require semantic-ui
+//= require_tree .
+
+$(function() {
+    $('.ui.dropdown').dropdown();
+})
