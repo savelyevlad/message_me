0b1ef148a33413af0e70dcdcae4089700a8325a7
Vladyslav <savelyevlad@gmail.com>
2023-03-05 18:19:54 +0100

    flash messages

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,3 +8,12 @@
 $(function() {
     $('.ui.dropdown').dropdown();
 })
+
+$('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  })
+;
