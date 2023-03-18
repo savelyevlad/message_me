7721b761f439611647f80ac97fae0162b0d1018e
Vladyslav <savelyevlad@gmail.com>
2023-03-18 20:00:23 +0100

    show 20 messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,6 @@ class Message < ApplicationRecord
   validates :body, length: { minimum: 1, maximum: 500 }
   has_one :user
   belongs_to :user
+
+  scope :custom_display, -> { order(:created_at).last(20) }
 end
